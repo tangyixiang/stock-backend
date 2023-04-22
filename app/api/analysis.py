d54492ef2942e2897f38ef083fc6ab0134dd9af4
tangyixiang <851565913@qq.com>
2023-04-22 15:56:29 +0800

    异常处理

--- app/api/analysis.py
+++ app/api/analysis.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 import pandas as pd
 import db.mysql as db
 import json
+import pandas_ta as ta
 
 router = APIRouter(prefix="/open")
 
@@ -29,12 +30,24 @@ async def kline_symbol(symbol: str):
     return convert_tojson(df)
 
 
+@router.get("/create/analysis/data")
+async def create_analysis_data():
+    new_placeholder = "%s" + ",%s" * 63
+    insert_sql = "INSERT INTO stock_analysis values( " + new_placeholder + ")"
 
-
-
-
-
-
-
-
-
+    data_sql = "select * from cn_stock_data where symbol = '{0}'"
+    query_sql = "select t.symbol from (select symbol from cn_stock_data group by symbol) t where t.symbol not in (SELECT symbol FROM stock_analysis GROUP BY symbol ) "
+    result = db.query(query_sql)
+    for data in result:
+        try:
+            sql = data_sql.format(data[0])
+            df = pd.read_sql(sql, db.pool.connection())
+            df2 = ta.cdl_pattern(df["open"], df["high"], df["low"], df["close"], name="all")
+            df = df.loc[:, ["date", "symbol"]]
+            df2["date"] = df["date"]
+            df_analysis = pd.merge(df, df2, on="date")
+            df_analysis = df_analysis.astype("str")
+            db.batchInsert(insert_sql, df_analysis.values.tolist())
+            print("插入完成")
+        except Exception as e:
+            print('异常',data[0])     
