42c2edc1bbe973100cc7a3e985ac53e9b2997984
tangyixiang <851565913@qq.com>
2023-05-20 21:50:32 +0800

    SQL优化

--- app/api/StockData.py
+++ app/api/StockData.py
@@ -2,8 +2,8 @@ import pandas as pd
 from fastapi import APIRouter, Depends
 from app.config.db import *
 from app.model.CnStockModel import *
-from sqlalchemy import select, text
 from sqlalchemy.orm import Session
+from operator import attrgetter
 
 router = APIRouter(prefix="/cn")
 
@@ -31,17 +31,17 @@ def vol_category(date: str, db: Session = Depends(getSesion)):
     return data
 
 
-# @router.get("/vol/up")
-# async def vol_up(date: str, industry: str):
-#     df = pd.read_sql(f"select a.*, b.market_value from cn_stock_vol_up a left join cn_stock_info b on a.symbol = b.symbol where a.date = '{date}' and a.industry = '{industry}' order by b.market_value asc ", dbpool.getconn())
-#     # data = df.to_json(orient="records", force_ascii=False)
-#     symbol_list = df["symbol"].values.tolist()
-#     data_list = []
-#     for symbol in symbol_list:
-#         sql = f"select * from (select * from cn_stock_data where symbol = '{symbol}' order by date desc limit 30) t order by date asc"
-#         data = query_for_obj(sql)
-#         row_data = list(df.loc[df["symbol"] == symbol].squeeze())
-#         name = row_data[2]
-#         market_value = row_data[len(row_data) - 1]
-#         data_list.append({"symbol": symbol, "name": name, "market_value": market_value, "data": data})
-#     return {"total": len(df), "list": data_list}
+@router.get("/vol/up")
+async def vol_up(date: str, industry: str, db: Session = Depends(getSesion)):
+    df = pd.read_sql(f"select a.*, b.market_value from cn_stock_vol_up a left join cn_stock_info b on a.symbol = b.symbol where a.date = '{date}' and a.industry = '{industry}' order by b.market_value asc ", engine.connect())
+    # data = df.to_json(orient="records", force_ascii=False)
+    symbol_list = df["symbol"].values.tolist()
+    data_list = []
+    for symbol in symbol_list:
+        data = db.query(CnStockData).where(CnStockData.symbol == symbol).order_by(CnStockData.date.desc()).limit(90).all()
+        data.sort(key=attrgetter('date'))
+        row_data = list(df.loc[df["symbol"] == symbol].squeeze())
+        name = row_data[2]
+        market_value = row_data[len(row_data) - 1]
+        data_list.append({"symbol": symbol, "name": name, "market_value": market_value, "data": data})
+    return {"total": len(df), "list": data_list}
